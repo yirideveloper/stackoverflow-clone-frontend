a4f8345573b07fa2ce3e87a902c9fd0e5898a0be
yirideveloper <topyiri1113@gmail.com>
2022-07-09 13:53:08 +0530

    Update README.md

--- src/redux/answers/answers.actions.js
+++ src/redux/answers/answers.actions.js
@@ -9,15 +9,13 @@ import axios from 'axios';
 import {setAlert} from '../alert/alert.actions';
 import config from "../../config";
 
-import censorBadWords from '../../services/censorBadWords'
-
 export const getAnswers = (id) => async (dispatch) => {
   try {
     const res = await axios.get(config.BASE_URL + `/api/posts/answers/${id}`);
 
     dispatch({
       type: GET_ANSWERS,
-      payload: res.data.data.map(({body, ...rest})=>({...censorBadWords({body}), ...rest})),
+      payload: res.data.data,
     });
   } catch (err) {
     dispatch({
